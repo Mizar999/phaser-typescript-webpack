8ccf51e4ff06aeed36d70baff2fc5147c9ac9371
Mario <mizar2502@aol.com>
2022-03-21 00:40:48 +0100

    Background stars don't overlap

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,10 +1,14 @@
 import "phaser";
 
 export class GameScene extends Phaser.Scene {
+    stars: { x: number, y: number }[];
+
     constructor() {
         super({
             key: "GameScene"
         });
+
+        this.stars = [];
     }
 
     preload(): void {
@@ -12,19 +16,33 @@ export class GameScene extends Phaser.Scene {
     }
 
     create(): void {
-        let count = Phaser.Math.Between(15, 20);
-        while(count > 0) {
+        let count = Phaser.Math.Between(30, 35);
+        while (count > 0) {
             this.addStar();
             count--;
         }
     }
 
     private addStar(): void {
-        let size = 28;
-        let x = Phaser.Math.Between(size / 2, 800 - size / 2);
-        let y = Phaser.Math.Between(size / 2, 600 - size / 2);
+        let size = 26;
         let id = Phaser.Math.Between(28, 31);
+        let y: number;
+        let x: number;
+        let alreadyExists: boolean;
+
+        do {
+            x = Phaser.Math.Between(size / 2, 800 - size / 2);
+            y = Phaser.Math.Between(size / 2, 600 - size / 2);
+            for (let element of this.stars) {
+                alreadyExists = Math.abs(x - element.x) < size && Math.abs(y - element.y) < size;
+                if (alreadyExists) {
+                    break;
+                }
+            }
+        } while (alreadyExists);
+
         let star = this.add.image(x, y, "space", id);
         star.setDisplaySize(size, size);
+        this.stars.push({ x: x, y: y })
     }
 }
